69c5afa82cd8c73e379ee9a9e4b53b9c5807a037
johnmat7 <joffyjoffy8@gmail.com>
2024-05-12 16:54:13 +0530

    pushing and getting data into db

--- blog/main.py
+++ blog/main.py
@@ -1,17 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends
 from pydantic import BaseModel
 import schemas , models 
 # from blog.database import engine
-import database
+# from .database import engine , SessionLocal
+from database import SessionLocal , engine , Base
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-models.database.Base.metadata.create_all(database.engine)
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal() 
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 
 
 @app.post("/blog")
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog , db : Session = Depends(get_db)):
+    new_blog = models.Blog(title = request.title , body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
 
+
+@app.get('/blog')
+def all(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def show(id , db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
 
 
