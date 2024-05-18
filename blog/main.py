e81105d50e1acb73f897c0176caa823dbaa8bbe2
johnmat7 <joffyjoffy8@gmail.com>
2024-05-18 12:26:58 +0530

    18th morning

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI , Depends
+from fastapi import FastAPI , Depends , status , Response , HTTPException
 from pydantic import BaseModel
 import schemas , models 
 # from blog.database import engine
 # from .database import engine , SessionLocal
 from database import SessionLocal , engine , Base
 from sqlalchemy.orm import Session
+import pprint
+
 
 app = FastAPI()
 
@@ -20,7 +22,7 @@ def get_db():
 
 
 
-@app.post("/blog")
+@app.post("/blog" , status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog , db : Session = Depends(get_db)):
     new_blog = models.Blog(title = request.title , body = request.body)
     db.add(new_blog)
@@ -35,8 +37,21 @@ def all(db:Session = Depends(get_db)):
     return blogs
 
 @app.get('/blog/{id}')
-def show(id , db:Session = Depends(get_db)):
+def show(id , response : Response, db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND , detail = f"Blog with the id {id}  not available")
+         
+    
     return blog
 
 
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+
+
+
+
+x = globals()
+pprint.pprint(x)
